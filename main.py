e0b90d847b913d53c9b3f7d9326de132e43a096a
Ohffs <ohffsnotnow@gmail.com>
2023-07-22 15:28:05 +0100

    Initial version

---
+++ main.py
@@ -0,0 +1,205 @@
+import os
+import re
+import urllib.parse
+from io import BytesIO
+import requests
+import time
+import argparse
+import json
+import PyPDF2
+import openai
+from youtube_transcript_api import YouTubeTranscriptApi
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+def print_info(message, quiet=False):
+    if quiet:
+        return
+    print(message)
+
+def get_prompt_from_file(filename):
+    try:
+        with open(filename) as f:
+            return f.read()
+    except FileNotFoundError:
+        return ''
+
+def get_prompt_text(prompt_name):
+    prompt = os.getenv(f'{prompt_name.upper()}_PROMPT', get_prompt_from_file(f'{prompt_name.lower()}_prompt.txt'))
+    if not prompt:
+        print(f'{prompt_name.upper()}_PROMPT not set and {prompt_name.lower()}_prompt.txt not found - using default prompt')
+        if prompt_name == 'summary':
+            prompt = "Could you summarise this text for me?"
+        elif prompt_name == 'sentiment':
+            prompt = "Could you tell me the sentiment of this text?"
+    return prompt
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai_model = os.getenv('OPENAI_MODEL', 'gpt-3.5-turbo-16k')
+summary_prompt = get_prompt_text('summary')
+sentiment_prompt = get_prompt_text('sentiment')
+
+if not openai.api_key:
+    print('You need to set the OPENAI_API_KEY environment variable.')
+    exit(1)
+
+def get_sentiment(text):
+    functions = [
+        {
+            "name": "get_sentiment_analysis",
+            "description": "this function is given an integer sentiment_score from 0 (very bad) to 10 (very good) and a short string sentiment_analysis which describes the result of the analysis.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "sentiment_score": {
+                        "type": "integer",
+                        "description": "The sentiment score from 0 (very bad) to 10 (very good).",
+                    },
+                    "sentiment_summary": {
+                        "type": "string",
+                        "description": "A short bit of text describing the sentiment analysis result.",
+                    },
+                },
+                "required": ["sentiment_score", "sentiment_summary"],
+            },
+        }
+    ]
+    function_call = {"name": "get_sentiment_analysis"}
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful AI assistant who specialises in sentiment analysis. You are given a piece of text and asked to analyse the sentiment."
+        },
+        {
+            "role": "user",
+            "content": f"{sentiment_prompt} :: {text}"
+        }
+    ]
+    return get_openai_response(messages, max_retries=5, functions=functions, function_call=function_call)
+
+def get_summary(text):
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful AI assistant who specialises in summarising text. You are given a piece of text and asked to summarise it."
+        },
+        {
+            "role": "user",
+            "content": f"{summary_prompt} :: {text}"
+        }
+    ]
+    return get_openai_response(messages, max_retries=5)
+
+def get_text_from_youtube(url):
+    video_id = url.split('watch?v=')[-1]
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript = transcript_list.find_transcript(['en'])
+        text = " ".join([i['text'] for i in transcript.fetch()])
+    except:
+        print(f"Could not get transcript for {url}")
+        exit(1)
+    return text
+
+def get_text_from_pdf(url):
+    try:
+        response = requests.get(url)
+        file = BytesIO(response.content)
+        pdf_reader = PyPDF2.PdfFileReader(file)
+        text = ""
+        for page_num in range(pdf_reader.getNumPages()):
+            page = pdf_reader.getPage(page_num)
+            text += page.extractText()
+        return text
+    except:
+        print(f"Could not get pdf text for {url}")
+        exit(1)
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def get_text_from_plain_url(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        texts = soup.findAll(string=True)
+        visible_texts = filter(tag_visible, texts)
+        return u" ".join(t.strip() for t in visible_texts)
+    except:
+        print(f"Could not get text for {url}")
+        exit(1)
+
+def get_text_from_url(url):
+    parsed = urllib.parse.urlparse(url)
+    if "youtube.com" in parsed.netloc:
+        return get_text_from_youtube(url)
+    elif re.search(r'\.pdf$', parsed.path, re.IGNORECASE):
+        return get_text_from_pdf(url)
+    else:
+        return get_text_from_plain_url(url)
+
+
+def get_openai_response(messages, max_retries=5, functions=None, function_call=None):
+    for i in range(max_retries):
+        try:
+             # Create a dictionary with the arguments for the create() method
+            kwargs = {
+                'model': openai_model,
+                'messages': messages
+            }
+
+            # Add the optional arguments to the dictionary if they are not None
+            if functions is not None:
+                kwargs['functions'] = functions
+            if function_call is not None:
+                kwargs['function_call'] = function_call
+
+            # Make the request to the API
+            response = openai.ChatCompletion.create(**kwargs)
+            if functions is not None:
+                return response.choices[0].message['function_call']
+            return response.choices[0].message['content']
+        except Exception as e:
+            if i < max_retries - 1:  # i is zero indexed
+                wait_time = (i+1) * 2  # exponential backoff
+                print(f"Error: {e}. Retrying in {wait_time} seconds...")
+                time.sleep(wait_time)
+            else:
+                raise  # re-throw the last exception if all retries fail
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some URL.')
+    parser.add_argument('url', type=str, help='The URL to process')
+    parser.add_argument('--max_tokens', type=int, default=3000, help='The maximum number of OpenAI tokens in a request')
+    parser.add_argument('--quiet', type=bool, default=False, help='Don\'t log anything - just print the response')
+
+    args = parser.parse_args()
+
+    url = args.url
+    max_tokens = args.max_tokens
+    quiet = args.quiet
+
+    print_info(f"Getting text from {url}", quiet)
+    text = get_text_from_url(url)
+    print_info(f"Got {len(text)} characters from {url}", quiet)
+
+    print_info(f"Getting summary of {len(text)} characters", quiet)
+    summary = get_summary(text)
+
+    print_info(f"Getting sentiment of {len(text)} characters", quiet)
+    sentiment = get_sentiment(text)
+    sentiment_dict = sentiment.to_dict()
+    # Parse the inner JSON structure
+    arguments = json.loads(sentiment_dict['arguments'])
+
+    print('Summary:')
+    print(summary)
+    print('Sentiment:')
+    print(f"Score: {arguments['sentiment_score']} || Analysis: {arguments['sentiment_summary']}")
+
+if __name__ == '__main__':
+    main()
